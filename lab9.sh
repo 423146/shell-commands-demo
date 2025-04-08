f66a33615bea55b6512c57961f9b6be12a95ac83
423146 <423146@student.nitandhra.ac.in>
2025-04-08 13:28:54 +0530

    Add files via upload

---
+++ lab9.sh
@@ -0,0 +1,17 @@
+# Installing a package using apt
+# Replace 'package-name' with the actual package (e.g., curl, vim, etc.)
+sudo apt install package-name
+
+# Upgrading a package
+# Replace 'package-name' with the actual package name
+sudo apt upgrade package-name
+
+# Removing a package using apt
+sudo apt remove package-name
+
+# Removing a package along with its configuration files
+sudo apt purge package-name
+
+# Updating the package list
+sudo apt update
+
