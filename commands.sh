533ae527d881676ab65dfb429f2a45a16289059c
423146 <423146@student.nitandhra.ac.in>
2025-04-08 12:58:50 +0530

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,15 @@
+# Create a directory
+mkdir my_directory
+
+# Create a subdirectory
+mkdir my_directory/my_subdirectory
+
+# Change to the subdirectory
+cd my_directory/my_subdirectory
+
+# Display the calendar
+cal
+
+# List the parent directory
+ls ..
+
