0d6ad9cfcbda2efa5f6bd5fc9867571ccc4d58cb
423146 <423146@student.nitandhra.ac.in>
2025-04-08 13:10:27 +0530

    Add files via upload

---
+++ lab1.sh
@@ -0,0 +1,40 @@
+# (i) Print the name of the operating system
+uname
+
+# (ii) Print the login name
+whoami
+
+# (iii) Print the host name
+hostname
+
+# 2. Display the calendar for
+
+# (i) January 2024
+cal 1 2024
+
+# (ii) February 1995
+cal 2 1995
+
+# (iii) 9th month of the year 7 A.D
+cal 9 7
+
+# (iv) Current month
+cal
+
+# (v) Current Date: Day Abbreviation, Month Abbreviation, and Year
+date "+%a %b %Y"
+
+# 3. Display the time in 12-Hour and 24-Hour Notations
+
+# 12-Hour Notation
+date "+%r"
+
+# 24-Hour Notation
+date "+%T"
+
+# 4. Display the Current Date and Current Time
+date
+
+# 5. Display the message “GOOD MORNING” in enlarged characters
+figlet "GOOD MORNING"
+
